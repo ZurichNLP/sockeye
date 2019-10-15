fb379f2a498caa908d041cd6463017f8c2dc49bd
Mathias Müller <mathias.mueller@uzh.ch>
2019-10-15 16:58:31 +0200

    more debug logging

--- sockeye/inference.py
+++ sockeye/inference.py
@@ -489,8 +489,8 @@ def models_max_input_output_length(models: List[InferenceModel],
     :return: The maximum input length and a function to get the output length given the input length.
     """
     logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH num_stds: %d" % num_stds)
-    logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH forced_max_input_len: %d" % forced_max_input_len)
-    logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH forced_max_output_len: %d" % forced_max_output_len)
+    logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH forced_max_input_len: %s" % str(forced_max_input_len))
+    logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH forced_max_output_len: %s" % str(forced_max_output_len))
 
     max_mean = max(model.length_ratio_mean for model in models)
     max_std = max(model.length_ratio_std for model in models)
@@ -506,9 +506,9 @@ def models_max_input_output_length(models: List[InferenceModel],
     logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH max_mean: %f" % max_mean)
     logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH max_std: %f" % max_std)
 
-    logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH supported_max_seq_len_source: %d" % supported_max_seq_len_source)
-    logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH supported_max_seq_len_target: %d" % supported_max_seq_len_target)
-    logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH training_max_seq_len_source: %d" % training_max_seq_len_source)
+    logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH supported_max_seq_len_source: %s" % supported_max_seq_len_source)
+    logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH supported_max_seq_len_target: %s" % supported_max_seq_len_target)
+    logging.info("MODELS_MAX_INPUT_OUTPUT_LENGTH training_max_seq_len_source: %s" % training_max_seq_len_source)
 
     return get_max_input_output_length(supported_max_seq_len_source,
                                        supported_max_seq_len_target,
