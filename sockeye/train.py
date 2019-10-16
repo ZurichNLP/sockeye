1afa2bfb604bdc5873e5d52f373828069c57215a
Mathias Müller <mathias.mueller@uzh.ch>
2019-10-16 12:19:18 +0200

    robust querying for GPU device memory etc.

--- sockeye/train.py
+++ sockeye/train.py
@@ -14,6 +14,15 @@
 """
 Simple Training CLI.
 """
+
+
+# Start the forkserver. It is important that this is done before any other imports so that the forkserver is in a clean
+# state.
+if __name__ == "__main__":
+    import sockeye.multiprocessing_utils as mp
+    mp.initialize()
+
+
 import argparse
 import os
 import shutil
